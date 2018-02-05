a3007850e239fc02e51107dc230ca19b4a9e8cde
Mavey Ma <maveyma@gmail.com>
2018-02-04 18:18:31 -0800

    User can pull to refresh the movie list.

--- flix/NowPlayingViewController.swift
+++ flix/NowPlayingViewController.swift
@@ -14,10 +14,23 @@ class NowPlayingViewController: UIViewController, UITableViewDataSource {
   @IBOutlet weak var tableView: UITableView!
   
   var movies: [[String: Any]] = []
+  var refreshControl: UIRefreshControl!
   
   override func viewDidLoad() {
     super.viewDidLoad()
+    
+    refreshControl = UIRefreshControl()
+    refreshControl.addTarget(self, action: #selector(NowPlayingViewController.didPullToRefresh(_:)), for: .valueChanged)
+    tableView.insertSubview(refreshControl, at: 0)
     tableView.dataSource = self
+    fetchMovies()
+  }
+  
+  @objc func didPullToRefresh(_ refreshControl: UIRefreshControl) {
+    fetchMovies()
+  }
+  
+  func fetchMovies() {
     // myMovieDatabaseAPIKey is hidden in Keys.plist
     var keys: NSDictionary?
     
@@ -45,6 +58,7 @@ class NowPlayingViewController: UIViewController, UITableViewDataSource {
           let movies = dataDictionary["results"] as! [[String: Any]]
           self.movies = movies
           self.tableView.reloadData()
+          self.refreshControl.endRefreshing()
         }
       }
       task.resume()
@@ -70,12 +84,6 @@ class NowPlayingViewController: UIViewController, UITableViewDataSource {
     cell.posterImageView.af_setImage(withURL: posterURL)
     return cell
   }
-  
-  
-  
-  
-  
-  
   
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
